50b0dc21283f9faad167e0ae76cf62673b6f0310
Troy <iranzithierry12@gmail.com>
2026-05-30 12:07:18 +0200

    improving UI

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 				<img src={viteLogo} className="vite" alt="Vite logo" />
 			</div>
 			<div>
-				<h1>React Vite Project</h1>
+				<h1>React/Vite Project</h1>
 			</div>
 		</section>
 	);
